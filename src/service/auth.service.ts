34fa0b305f624a4bf1246f761f52f9341e7b2c64
revv2703 <asijaprakash27@gmail.com>
2023-11-12 22:25:34 +0530

    Added JWT implementation and session support

---
+++ src/service/auth.service.ts
@@ -0,0 +1,39 @@
+import { DocumentType } from "@typegoose/typegoose";
+import { User, privateFields } from "../model/user.model";
+import { signJwt } from "../utils/jwt";
+import SessionModel from "../model/session.model";
+import { omit } from 'lodash'
+
+
+export async function createSession({userId}: {userId: string}) {
+    return SessionModel.create({user: userId})
+}
+
+
+export async function signRefreshToken({userId}: {userId: string}) {
+    const session = await createSession({
+        userId
+    })
+
+    const refreshToken = signJwt({
+        session: session._id
+    }, "refreshTokenPrivateKey", {
+        expiresIn: "1y"
+    })
+
+    return refreshToken
+}
+
+export function signAccessToken(user: DocumentType<User>){
+    const payload = omit(user.toJSON(), privateFields)
+
+    const accessToken = signJwt(payload, "accessTokenPrivateKey", {
+        expiresIn: "15m"
+    })
+
+    return accessToken
+}
+
+export async function findSessionById(id: string) {
+    return SessionModel.findById(id)
+}
